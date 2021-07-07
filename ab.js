bc190b0205709b87e7a96dd61b988d1642939983
FreeTricks7 <79830088+FreeTricks7@users.noreply.github.com>
2021-07-07 11:14:27 +0100

    Add files via upload

---
+++ ab.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+const url = new URL(window.location);
+if(url.pathname == "/mobile/tecno-spark-7")){
+var link = "https://mobilesworld.page.link?apn=com.pakistan.mobilesworld&link=https%3A%2F%2Fmobilesworld.pk" + encodeURI(url.pathname);
+window.location = link;}
+</script>
